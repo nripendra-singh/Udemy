f13b81a9ef92055df055fb0185bdeebe179bd62b
nripendra-singh <nripendra91@gmail.com>
2021-05-07 23:45:43 +0100

    Assignment 2 Razor View
    My attempt at Razor View Assignment

---
+++ C# Training/Section 5 Razor View Engine/Razor Assignment/Razor Assignment/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home/Index
+        public ActionResult Index(int? amount)
+        {
+            ViewBag.amount = amount;
+            return View();
+
+        }
+    }
+}
+
